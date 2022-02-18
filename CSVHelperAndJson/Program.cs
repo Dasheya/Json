c814891a285938036dae7572fe20f9199126714f
Darshan Thorat <darshaanthorat17@gmail.com>
2022-02-18 11:36:09 +0530

    JsonProblem

---
+++ CSVHelperAndJson/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSVHelperAndJson;
+
+Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("# Read data from CSV & Write data in CSV");
+//CSVHandler.ImplementCSVDataHandling();
+ReadCSVWriteCSV.ImplementCSVToJSON();
+Console.ReadKey();
